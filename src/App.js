77149f057d4dd793a33a0c980e94e78a89ce6337
Juan Ocaña <juanov98@gmail.com>
2021-04-20 19:31:42 +0200

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './app.scss';
+import { Content } from 'carbon-components-react';
+import { Route, Switch } from 'react-router-dom';
+import TutorialHeader from './components/TutorialHeader';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <>
+      <TutorialHeader />
+      <Content>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/repos" component={RepoPage} />
+        </Switch>
+      </Content>
+    </>
+  );
 }
 
 export default App;
